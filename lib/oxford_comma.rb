e988e2f3d5756dec542da66da3d80c20d89e9079
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 00:22:06 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  formatted_string = ""
+  array.each_with_index { |e,index|
+    if(index==0)
+      formatted_string << e
+    elsif(index>0 && index<(array.length - 1))
+      formatted_string << ", #{e}"
+    else
+      formatted_string << " and #{e}"
+  }
 end
